f2ebc1076c9c6ba8b4bc2f77b3899724f9d69d72
sambetant <104916257+sambetant@users.noreply.github.com>
2023-01-13 15:44:56 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.rotation(Rotation.Pitch) > 10) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.rotation(Rotation.Pitch) < -10) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
